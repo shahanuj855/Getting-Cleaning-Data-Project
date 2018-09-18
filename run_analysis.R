3e6e98f074ddff1be92ae7c609c5a18d0e477fa0
Anuj Shah <shahanuj855@gmail.com>
2018-09-18 17:47:31 -0400

    Reuploading All Files

--- run_analysis.R
+++ run_analysis.R
@@ -7,30 +7,30 @@ library(data.table)
 features <- read.csv('./UCI HAR Dataset/features.txt', header = FALSE,sep = ' ')
 features <- as.character(features[,2])
 
-data.train.x <- read.table('./UCI HAR Dataset/train/X_train.txt')
-data.train.activity <- read.csv('./UCI HAR Dataset/train/y_train.txt',header = FALSE,sep = ' ')
-data.train.subject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE,sep = " ")
+traindata_x <- read.table('./UCI HAR Dataset/train/X_train.txt')
+traindata_activity <- read.csv('./UCI HAR Dataset/train/y_train.txt',header = FALSE,sep = ' ')
+traindata_subject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE,sep = " ")
 
-data.train <- data.frame(data.train.subject,data.train.activity,data.train.x)
-names(data.train) <- c(c('subject','activity'),features)
+train_data <- data.frame(traindata_subject,traindata_activity,traindata_x)
+names(train_data) <- c(c('subject','activity'),features)
 
-data.test.x <- read.table('./UCI HAR Dataset/test/X_test.txt')
-data.test.activity <- read.csv('./UCI HAR Dataset/test/y_test.txt',header = FALSE,sep = ' ')
-data.test.subject <- read.csv('./UCI HAR Dataset/test/subject_test.txt',header = FALSE,sep = ' ')
+testdata_x <- read.table('./UCI HAR Dataset/test/X_test.txt')
+testdata_activity <- read.csv('./UCI HAR Dataset/test/y_test.txt',header = FALSE,sep = ' ')
+testdata_subject <- read.csv('./UCI HAR Dataset/test/subject_test.txt',header = FALSE,sep = ' ')
 
-data.test <- data.frame(data.test.subject, data.test.activity, data.test.x)
-names(data.test) <- c(c('subject','activity'),features)
+test_data <- data.frame(testdata_subject, testdata_activity, testdata_x)
+names(test_data) <- c(c('subject','activity'),features)
 
-data.all <- rbind(data.train,data.test) #merge the training and testing dataset
+data.all <- rbind(train_data,test_data) #merge the training and testing dataset
 
-mean_std.select <- grep('mean|std',features)
-data.sub <- data.all[,c(1,2,mean_std.select + 2)]
+sub_meanStd <- grep('mean|std',features)
+sub_data <- data.all[,c(1,2,sub_meanStd + 2)]
 
 activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
 activity.labels <- as.character(activity.labels[,2])
 data.sub$activity <- activity.labels[data.sub$activity]
 
-name.new <- names(data.sub)
+name.new <- names(sub_data)
 name.new <- gsub("[(][)]", "", name.new)
 name.new <- gsub("^t", "TimeDomain_", name.new)
 name.new <- gsub("^f", "FrequencyDomain_", name.new)
@@ -40,7 +40,7 @@ name.new <- gsub("Mag", "Magnitude", name.new)
 name.new <- gsub("-mean-", "_Mean_", name.new)
 name.new <- gsub("-std-", "_StandardDeviation_", name.new)
 name.new <- gsub("-", "_", name.new)
-names(data.sub) <- name.new
+names(sub_data) <- name.new
 
-data.tidy <- aggregate(data.sub[,3:81], by = list(activity = data.sub$activity, subject = data.sub$subject),FUN = mean)
+data.tidy <- aggregate(sub_data[,3:81], by = list(activity = sub_data$activity, subject = sub_data$subject),FUN = mean)
 write.table(x = data.tidy, file = "data_tidy.txt", row.names = FALSE)
